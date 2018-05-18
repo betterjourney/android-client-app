9f189891340b32dba4f6de3b4418c11cbf65bdaa
av-7 <amanshulvyas@gmail.com>
2018-05-18 05:14:07 -0700

    Adds Navigation Activity

---
+++ source/app/src/main/java/com/betterjourney/android/pilot/ui/NavigationActivity.java
@@ -0,0 +1,46 @@
+package com.betterjourney.android.pilot.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.betterjourney.android.pilot.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_messages:
+                    mTextMessage.setText(R.string.title_messages);
+                    return true;
+                case R.id.navigation_journeys:
+                    mTextMessage.setText(R.string.title_journeys);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
