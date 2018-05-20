0a5650da6548750e96348b8796301596295ea920
av-7 <amanshulvyas@gmail.com>
2018-05-20 15:56:38 -0700

    Populates Journey View with mock data

--- source/app/src/main/java/com/betterjourney/android/pilot/ui/journeys/JourneysViewFragment.java
+++ source/app/src/main/java/com/betterjourney/android/pilot/ui/journeys/JourneysViewFragment.java
@@ -2,6 +2,8 @@ package com.betterjourney.android.pilot.ui.journeys;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,12 @@ import com.betterjourney.android.pilot.R;
 
 public class JourneysViewFragment extends Fragment {
 
+    String[] mDemoData  = {"Journey 1", "Journey 2", "Journey 3", "Journey 4", "Journey 5", "Journey 6", "Journey 7", "Journey 8",
+            "Journey 9", "Journey 10", };
+
+
     public static JourneysViewFragment newInstance() {
-        JourneysViewFragment journeysViewFragment = new JourneysViewFragment();
-        return journeysViewFragment;
+        return new JourneysViewFragment();
     }
 
     @Override
@@ -22,7 +27,19 @@ public class JourneysViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_journeys_view, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_journeys_view, container, false);
+        RecyclerView mRecyclerView = rootView.findViewById(R.id.rv_journeys);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getActivity(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+       JourneysViewAdapter mJourneysViewAdapter = new JourneysViewAdapter(this.getActivity());
+        mJourneysViewAdapter.setJourneysData(mDemoData);
+        mRecyclerView.setAdapter(mJourneysViewAdapter);
+
+        return rootView;
     }
 
 //    private RecyclerView mRecyclerView;
