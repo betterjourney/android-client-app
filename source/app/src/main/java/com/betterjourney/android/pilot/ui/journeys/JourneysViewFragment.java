d4e6d5773515b5f0c036c8fab27b7018eb4a1670
av-7 <amanshulvyas@gmail.com>
2018-05-19 22:50:53 -0700

    Adds working navigation view for all the fragments

--- source/app/src/main/java/com/betterjourney/android/pilot/ui/journeys/JourneysViewFragment.java
+++ source/app/src/main/java/com/betterjourney/android/pilot/ui/journeys/JourneysViewFragment.java
@@ -1,17 +1,10 @@
-package com.betterjourney.android.pilot.ui.goals;
+package com.betterjourney.android.pilot.ui.journeys;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.betterjourney.android.pilot.R;
 
 
@@ -31,12 +24,6 @@ public class JourneysViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_journeys_view, container, false);
     }
-
-
-
-
-
-
 
 //    private RecyclerView mRecyclerView;
 //
