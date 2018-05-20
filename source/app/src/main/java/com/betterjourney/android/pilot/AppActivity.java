12e7506798030e2a52c3f3fbc1b0f65324f4c3cc
av-7 <amanshulvyas@gmail.com>
2018-05-19 22:52:50 -0700

    Adds new primary color theme to the whole application

--- source/app/src/main/java/com/betterjourney/android/pilot/AppActivity.java
+++ source/app/src/main/java/com/betterjourney/android/pilot/AppActivity.java
@@ -3,9 +3,15 @@ package com.betterjourney.android.pilot;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.betterjourney.android.pilot.ui.journeys.JourneysViewFragment;
+import com.betterjourney.android.pilot.ui.home.HomeViewFragment;
+import com.betterjourney.android.pilot.ui.messages.MessagesViewFragment;
 
 public class AppActivity extends AppCompatActivity {
 
@@ -16,18 +22,25 @@ public class AppActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_messages:
-                    mTextMessage.setText(R.string.title_messages);
-                    return true;
-                case R.id.navigation_journeys:
-                    mTextMessage.setText(R.string.title_journeys);
-                    return true;
+                case R.id.navigation_home_view:
+//                    mTextMessage.setText(R.string.title_home);
+                    selectedFragment = HomeViewFragment.newInstance();
+                    break;
+                case R.id.navigation_messages_view:
+                    //mTextMessage.setText(R.string.title_messages);
+                    selectedFragment = MessagesViewFragment.newInstance();
+                    break;
+                case R.id.navigation_journeys_view:
+                    //mTextMessage.setText(R.string.title_journeys);
+                    selectedFragment = JourneysViewFragment.newInstance();
+                    break;
             }
-            return false;
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_frame_layout, selectedFragment);
+            fragmentTransaction.commit();
+            return true;
         }
     };
 
@@ -37,8 +50,12 @@ public class AppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_app);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation_bottom_view);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_frame_layout, HomeViewFragment.newInstance());
+        fragmentTransaction.commit();
     }
 
 }
